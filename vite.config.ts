792620761e912c701eea4ef9481d14c8aa51275f
cezar-4blue <cezar@4blue.com.br>
2026-08-12 18:34:13 -0300

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '');
+  const base = process.env.GITHUB_REPOSITORY
+    ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/`
+    : './';
+  return {
+    plugins: [react(), tailwindcss()],
+    base,
+    define: { 'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY) },
+    resolve: { alias: { '@': path.resolve(__dirname, './src') } },
+    server: { hmr: process.env.DISABLE_HMR !== 'true' },
+  };
+});
